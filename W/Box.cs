48852842e8f89966871ca026627f214d04f94246
odeth <odeth@AYELLEN>
2024-02-18 17:19:29 -0600

    Agregar archivos de proyecto.

---
+++ W/Box.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace W
+{
+	internal class Box //Class Box= Any object that has mass
+	{
+        //Properties. Object characteristics
+        protected double GRAVITY = 9.81;
+		protected double mass; // It is a field, saves the data as a variable
+
+        //Propierty
+        public double Mass 
+        {
+			get { return mass; } //You can make a property read-only or write-only by making it private.
+            set { mass = value; }
+		}
+
+		//Constructor. To initialize the variables in a class
+		public Box()
+		{
+			mass = 0;
+		}
+
+		//Method
+		public double CalculateWeight()
+		{
+			return mass*GRAVITY;
+		}
+
+	}
+}
---
+++ W/Program.cs
@@ -0,0 +1,16 @@
+﻿
+using System;
+namespace W
+{
+class Program
+    {
+        static void Main(string[] args) 
+        {
+            Box box1 = new Box();
+            box1.Mass = 35;
+            Console.WriteLine("Mass:"+box1.Mass);
+
+            Console.WriteLine("The weight of the object is:"+box1.CalculateWeight()+" N");
+        }
+    }
+}
